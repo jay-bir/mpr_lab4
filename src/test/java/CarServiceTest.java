f2470bdc5b3b49dfe03e12aea65452e7b42a434a
jay-bir <johnxpw@gmail.com>
2020-11-19 11:27:11 +0100

    added test for CarService registerClientCar

---
+++ src/test/java/CarServiceTest.java
@@ -0,0 +1,58 @@
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CarServiceTest {
+
+    @Mock
+    CarCreator carCreator;
+
+    @Mock
+    ClientRepository clientRepository;
+
+    @Mock
+    PaymentRepository paymentRepository;
+
+
+    public void registerClientCarTestShouldSetCarToClient(){
+        String clientId = "12345";
+        Client client = new Client("12345",true, null);
+
+        ArgumentCaptor<Client> captor = ArgumentCaptor.forClass(Client.class);
+
+        String carMake = "FastestCars";
+        String carColor = "Red";
+        String carPlate = "54321";
+        Car car = new Car();
+        car.setMake(carMake);
+        car.setColor(carColor);
+        car.setPlate(carPlate);
+
+        when(carCreator.createCar(carMake,carColor,carPlate)).thenReturn(car);
+        when(clientRepository.findClient(clientId)).thenReturn(client);
+
+        CarService carService = new CarService(carCreator,clientRepository,paymentRepository);
+        Client returnedClient = carService.registerClientCar(clientId,carMake,carColor,carPlate);
+
+        verify(clientRepository).saveClient(captor.capture());
+
+        assertEquals(clientId,returnedClient.getId());
+        assertTrue(returnedClient.isActive());
+        assertEquals(carMake,returnedClient.getCar().getMake());
+        assertEquals(carColor,returnedClient.getCar().getColor());
+        assertEquals(carPlate,returnedClient.getCar().getPlate());
+
+        assertEquals(clientId,captor.getValue().getId());
+        assertEquals(carMake,captor.getValue().getCar().getMake());
+        assertEquals(carColor,captor.getValue().getCar().getColor());
+        assertEquals(carPlate,captor.getValue().getCar().getPlate());
+
+    }
+}
--- src/test/java/ClientServiceTest.java
+++ src/test/java/ClientServiceTest.java
@@ -1,2 +1,92 @@
-package PACKAGE_NAME;public class ClientServiceTest {
+import exception.ClientAlreadyInactiveException;
+import exception.ClientNotFoundException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ClientServiceTest {
+
+    @Mock
+    ClientRepository clientRepository;
+
+
+    @Test
+    public void disableClientAccountMakesClientInactiveAndSavesToDbWhenClientIsFoundAndIsActive(){
+        String id = "12345";
+        ClientService clientService = new ClientService(clientRepository);
+        Client client = new Client(id,true,null);
+
+        when(clientRepository.findClient(id)).thenReturn(client);
+        doAnswer(invocation ->{
+            Client c = invocation.getArgument(0);
+            assertFalse(c.isActive());
+            return null;
+        }).when(clientRepository).saveClient(any());
+
+        Client returnedClient = clientService.disableClientAccount(id);
+
+        verify(clientRepository).saveClient(client);
+
+
+        assertEquals(id, returnedClient.getId());
+        assertFalse(returnedClient.isActive());
+
+    }
+
+
+    @Test(expected = ClientNotFoundException.class)
+    public void disableClientAccountThrowsExceptionWhenClientNotFound(){
+        String id = "12345";
+        ClientService clientService = new ClientService(clientRepository);
+
+        when(clientRepository.findClient(id)).thenReturn(null);
+
+        clientService.disableClientAccount(id);
+    }
+
+    @Test(expected = ClientAlreadyInactiveException.class)
+    public void disableClientAccountThrowsExceptionWhenClientAlreadyInactive(){
+        String id = "12345";
+        ClientService clientService = new ClientService(clientRepository);
+        Client client = new Client(id,false,null);
+
+        when(clientRepository.findClient(id)).thenReturn(client);
+
+        clientService.disableClientAccount(id);
+    }
+
+    @Test
+    public void updateClientAccountIdTestWithClientExpectedClientWithNewId(){
+        String id ="12345";
+        String newId = "54321";
+        Client client = new Client(id,true,null);
+        ClientService clientService = new ClientService(clientRepository);
+
+        when(clientRepository.findClient(id)).thenReturn(client);
+
+        Client returnedClient = clientService.updateClientAccountId(id,newId);
+
+        verify(clientRepository).saveClient(client);
+        verify(clientRepository).deleteClient(id);
+
+        assertEquals(newId,returnedClient.getId());
+        assertTrue(returnedClient.isActive());
+        assertNull(returnedClient.getCar());
+    }
+
+    @Test(expected = ClientNotFoundException.class)
+    public void updateClientAccountIdWithNoClient(){
+        String id = "12345";
+        String newId = "54321";
+        ClientService clientService = new ClientService(clientRepository);
+
+        when(clientRepository.findClient(id)).thenReturn(null);
+
+        clientService.updateClientAccountId(id,newId);
+    }
 }
